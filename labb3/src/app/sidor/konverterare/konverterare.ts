b668fe95f335381ddc3bd45f06062e371d6b1eaf
aliatwood <aliatwood0104@gmail.com>
2026-04-26 15:09:53 +0200

    uppdatera konverterare css,html,ts
    la till konverteringssida med funktioner och styling

--- labb3/src/app/sidor/konverterare/konverterare.ts
+++ labb3/src/app/sidor/konverterare/konverterare.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-konverterare',
-  imports: [],
   templateUrl: './konverterare.html',
-  styleUrl: './konverterare.css',
+  styleUrls: ['./konverterare.css'],
+  imports: [FormsModule]
 })
-export class KonverterareComponent {}
+export class KonverterareComponent {
+
+  meter: number = 0;
+  feet: number = 0;
+
+  celsius: number = 0;
+  fahrenheit: number = 0;
+
+  meterToFeet() {
+    this.feet = this.meter * 3.28084;
+  }
+
+  feetToMeter() {
+    this.meter = this.feet / 3.28084;
+  }
+
+  celsiusToFahrenheit() {
+    this.fahrenheit = (this.celsius * 9 / 5) + 32;
+  }
+
+  fahrenheitToCelsius() {
+    this.celsius = (this.fahrenheit - 32) * 5 / 9;
+  }
+}
